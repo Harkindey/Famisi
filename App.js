ea8b4216d49e0893346bfad35023e4cf0d3dcada
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-11-10 09:48:06 +0100

    The styling of Game Area

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Main from './src/Main'
+import Main from './src/Main';
 
 export default class App extends React.Component {
   render() {
--- src/Main.js
+++ src/Main.js
@@ -1,53 +1,58 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements'
+import Dimensions from 'Dimensions';
+import Result from './Result'
+
+const x = Dimensions.get('window').width;
+const y = Dimensions.get('window').height;
 
 import db from './db';
 
- const Question =  () =>  {
-   return (
-     <View style={{flex: 1}}>
-        {db.map((item, index) => {
-          console.log(item, index);
-          return(
-              <View key={index} style={{ flex: 1}}>
-                <Text>ABURO</Text>
-                <Text>A Younger One</Text>
-                <View style={{flex: 1, flexDirection: 'column'}>
-                  <View style={{flex: 1, flexDirection: 'row'}}>
-                    <Button 
-                    raised
-                    title='àbúrò'
-                    />
-                    <Button 
-                    raised
-                    title='aburo'
-                    />
-                  </View>
-                  <View style={{flex: 1, flexDirection: 'row'}}>
-                  <Button 
-                  raised
-                  title='ábúrọ'
-                  />
-                  <Button 
-                  raised
-                  title='àbùrọ̀'
-                  />
-                  </View>
-                </View>
-              </View>
-          )
-        })}
-     </View>
-   )
- }
-
 class Main extends React.Component {
+
+  checkAnswer = () => {
+    console.log('bean');
+    return(
+      <View style={{flex: 1 , backgroundColor: 'rgba(0,0,0,0.2)', justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Correct</Text>
+      </View>
+    )
+  }
   render() {
     return (
       <View style={styles.container}>
-       <Question />
-        <Text>Open up App.js to start working on your app!</Text>
+              <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column',}}>
+                <Text style={styles.word}>ABURO</Text>
+                <Text style={{fontSize: 20, paddingBottom: x * 0.5}}>A Younger One</Text>
+                <View style={{flexDirection: 'column', paddingTop: 50}}>
+                  <View style={styles.innerStyle}>
+                      <Button 
+                      buttonStyle={styles.buttonStyle}
+                      large
+                      backgroundColor="#1EB9B9"
+                      title='àbúrò'
+                      onPress={this.checkAnswer}
+                      />
+                  </View>
+                  <View style={styles.innerStyle}>
+                      <Button
+                      large
+                      buttonStyle={styles.buttonStyle}
+                      backgroundColor="#1EB9B9"
+                      title='aburo'
+                      />
+                  </View>
+                  <View style={styles.innerStyle}>
+                      <Button 
+                      buttonStyle={styles.buttonStyle}
+                      large
+                      backgroundColor="#1EB9B9"
+                      title='ábúrọ'
+                      />
+                  </View>
+                </View>      
+                </View>
       </View>
     );
   }
@@ -60,6 +65,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  word: {
+    color: '#1EB9B9',
+    fontWeight: 'bold',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  red: {
+    color: 'red',
+  },
+  buttonStyle: {
+    borderRadius: 10,
+    width: x * 0.8,
+  },
+  innerStyle: {
+    padding: 10
+  }
 });
 
 export default  Main
