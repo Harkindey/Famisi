90da0ad9a8308bd1c931432d2dff6cac466aeb37
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2018-02-13 05:33:47 +0100

    remove EXPO, changed to react-native cli

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Modal} from 'react-native';
+import { StyleSheet, Text, View, Modal } from 'react-native';
 import { Button } from 'react-native-elements'
 import Dimensions from 'Dimensions';
 
@@ -8,111 +8,113 @@ const y = Dimensions.get('window').height;
 
 
 class Main extends React.Component {
-    constructor(props){
-      super(props);
-      this.state ={
-        question : this.props.data,
-        color: this.props.color,
-        score: 0,
-        ismodalVisible: false,
-        finished: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: this.props.data,
+      color: this.props.color,
+      score: 0,
+      ismodalVisible: false,
+      finished: false
+    }
+  }
+
+  componentWillMount() {
+  }
+
+  renderResult() {
+    let { show, finished, question, color, score } = this.state
+    return (
+      <View style={{ top: y * 0.5, left: x * 0.5 }}>
+        <Modal
+          animationType="fade"
+          transparent={false}
+          visible={show}
+          onRequestClose={() => { alert("Modal has been closed.") }}
+        >
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.4)' }}>
+            <View>
+              <Text style={{ fontWeight: 'bold', fontSize: 30, fontWeight: 'bold' }}>{
+                (finished) ? 'FINAL SCORE' : 'GAME OVER'
+              }</Text>
+              <Text style={{ fontWeight: 'bold', fontSize: 30, fontWeight: 'bold' }}>SCORE: {score}</Text>
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+              <Button
+                buttonStyle={{ borderRadius: 10, width: x * 0.4 }}
+                large
+                backgroundColor='black'
+                title='Replay'
+                onPress={() => this.setState({
+                  question: this.props.data,
+                  color: color,
+                  ismodalVisible: false,
+                  score: 0,
+                  finished: false
+                })}
+              />
+            </View>
+          </View>
+        </Modal>
+      </View>
+    )
+  }
+
+  renderQuestion() {
+    console.log(this.state);
+    let { question, color, score } = this.state
+    const index = Math.floor(Math.random() * question.length);
+    const colorGroup = Math.floor(Math.random() * 50);
+    const colorindex = Math.floor(Math.random() * 5);
+    const colors = color[colorGroup];
+
+    const checkAnswer = (id) => {
+      if (question[index].correctIndex === id) {
+        if (question.length > 1) {
+          console.log('Correct Answer')
+          question.splice(index, 1)
+          this.setState({
+            question,
+            score: score += 1
+          })
+          // console.log(score);
+        } else {
+          this.setState({
+            score: score += 1,
+            finished: true,
+            ismodalVisible: true
+          })
+        }
+      } else {
+        console.log(score);
+        console.log('Wrong Answer')
+        this.setState({ ismodalVisible: true });
       }
     }
-
-    componentWillMount(){    
-    }
-
-    renderResult(){
-      let { show, finished, question, color, score } = this.state
-      return (
-        <View style={{top:y * 0.5, left:x * 0.5}}>
-        <Modal
-         animationType="fade"
-         transparent={false}
-         visible={show}
-         onRequestClose={() => {alert("Modal has been closed.")}}
-         >
-           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.4)'}}>
-             <View>
-               <Text style={{fontWeight: 'bold', fontSize: 30, fontWeight: 'bold'}}>{
-                 (finished) ? 'FINAL SCORE' : 'GAME OVER'  
-               }</Text>
-               <Text style={{fontWeight: 'bold', fontSize: 30, fontWeight: 'bold'}}>SCORE: {score}</Text>
-             </View>
-             <View style={{flexDirection: 'row'}}>
-             <Button 
-                     buttonStyle={{borderRadius: 10, width: x * 0.4}}
-                     large
-                     backgroundColor='black'
-                     title='Replay'
-                     onPress={() => this.setState({
-                       question : question,
-                       color: color,
-                       ismodalVisible: false
-                     })}
-                     />
-           </View>
-           </View>
-         </Modal>
-       </View>
-      )
-    }
-  
-    renderQuestion() {
-      console.log(this.state);
-      let { question, color, score  } = this.state
-      const index = Math.floor(Math.random() * question.length);
-      const colorGroup= Math.floor(Math.random() * 50);
-      const colorindex = Math.floor(Math.random() * 5);
-      const colors = color[colorGroup];
-
-      const checkAnswer = (id) => { 
-          if (question[index].correctIndex === id){
-              if(question.length > 1){
-                    console.log('Correct Answer')
-                    question.splice(index, 1)
-                    this.setState({
-                      question,
-                      score: score+=1
-                    })
-                  // console.log(score);
-                  } else {
-                  this.setState({
-                    score:  score+=1,
-                    finished: true,
-                    ismodalVisible: true
-                  })
-                }
-          }else {
-            console.log(score);
-            console.log('Wrong Answer')
-            this.setState({ismodalVisible: true});
-          }
-        }
-      return(
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column',}}>
-          <Text style={[styles.word,{color: colors[colorindex]}]}>{question[index].word}</Text>
-          <Text style={{fontSize: 20, paddingBottom: x * 0.5}}>{question[index].meaning}</Text>
-          <View style={{flexDirection: 'column', justifyContent:'flex-end'}}>
-          {question[index].options.map((item, index)=> {
-            return(
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'column', }}>
+        <Text style={[styles.word, { color: colors[colorindex] }]}>{question[index].word}</Text>
+        <Text style={{ fontSize: 20, paddingBottom: x * 0.5 }}>{question[index].meaning}</Text>
+        <View style={{ flexDirection: 'column', justifyContent: 'flex-end' }}>
+          {question[index].options.map((item, index) => {
+            return (
               <View style={styles.innerStyle} key={index}>
-                <Button 
-                buttonStyle={styles.buttonStyle}
-                large
-                backgroundColor={colors[colorindex]}
-                title={item}
-                onPress={checkAnswer.bind(null,index)}
+                <Button
+                  buttonStyle={styles.buttonStyle}
+                  large
+                  backgroundColor={colors[colorindex]}
+                  title={item}
+                  onPress={checkAnswer.bind(null, index)}
                 />
               </View>
-              )
+            )
           })}
-          </View>      
-          </View>
-      )
-    }
+        </View>
+      </View>
+    )
+  }
   render() {
-    if(this.state.ismodalVisible){
+    if (this.state.ismodalVisible) {
       return (
         <View>
           {this.renderResult()}
@@ -120,9 +122,9 @@ class Main extends React.Component {
       )
     }
     return (
-      <View style={{flex:1}}>     
+      <View style={{ flex: 1 }}>
         <View style={styles.container}>
-          {this.renderQuestion()}   
+          {this.renderQuestion()}
         </View>
       </View>
     );
@@ -155,4 +157,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default  Main
+export default Main
--- src/Result.js
+++ src/Result.js
@@ -10,30 +10,30 @@ class Result extends Component {
   }
 
   setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+    this.setState({ modalVisible: visible });
   }
 
   render() {
     return (
-      <View style={{marginTop: 22}}>
+      <View style={{ marginTop: 22 }}>
         <Modal
           animationType="slide"
           transparent={false}
           visible={this.state.modalVisible}
-          onRequestClose={() => {alert("Modal has been closed.")}}
-          >
-         <View style={{marginTop: 22}}>
-          <View>
-            <Text>Hello World!</Text>
+          onRequestClose={() => { alert("Modal has been closed.") }}
+        >
+          <View style={{ marginTop: 22 }}>
+            <View>
+              <Text>Hello World!</Text>
 
-            <TouchableHighlight onPress={() => {
-              this.setModalVisible(!this.state.modalVisible)
-            }}>
-              <Text>Hide Modal</Text>
-            </TouchableHighlight>
+              <TouchableHighlight onPress={() => {
+                this.setModalVisible(!this.state.modalVisible)
+              }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
 
+            </View>
           </View>
-         </View>
         </Modal>
 
         <TouchableHighlight onPress={() => {
